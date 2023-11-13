c0818beed763a7d35e892e8a7f6236ae523d2f3a
srividya9642 <bellamkondasrividya889771661@gmail.com>
2023-11-13 17:14:59 +0530

    learn_react

---
+++ my-react-app/src/My_first_work/Component_1.jsx
@@ -0,0 +1,7 @@
+import React,{Component} from "react";
+export default function Component_1(){
+    return
+    <>
+    <h1>my first react app</h1>
+    </>
+}
---
+++ my-react-app/src/index.js
@@ -0,0 +1,3 @@
+import ReactDom from 'react-dom'
+//ReactDom.render(1,2);
+ReactDom. render(<div><h1>hello ,GM</h1><h2>welcome to react</h2></div>, document.getElementById("rajinikanth"))
